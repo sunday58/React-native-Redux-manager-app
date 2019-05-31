1c4b88b49ea8c967615a52827edbafe592cf6b24
sunday58 <46400048+sunday58@users.noreply.github.com>
2019-05-31 16:09:23 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ import ignoreWarnings from 'react-native-ignore-warnings';
 
 class App extends Component {
     componentWillMount () {
-        const config = {
-            apiKey: 'AIzaSyDpTwn1dePRWFMMjTaudkHbWMkAAKLj7oU',
-            authDomain: 'manager-b28d7.firebaseapp.com',
-            databaseURL: 'https://manager-b28d7.firebaseio.com',
-            projectId: 'manager-b28d7',
-            storageBucket: 'manager-b28d7.appspot.com',
-            messagingSenderId: '296951571290'
+        
             
           };
           firebase.initializeApp(config);
